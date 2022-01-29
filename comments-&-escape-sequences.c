46faf6fdbdd8c003cdfe38cb1dfdc206c88104f7
00300 <90952986+well300@users.noreply.github.com>
2022-01-29 18:28:18 +0530

    comments & escape sequences 🍳

---
+++ comments-&-escape-sequences.c
@@ -0,0 +1,23 @@
+//comments & escape sequences 🍳
+
+#include <stdio.h> 
+
+int main(){
+   //This is a comment blah
+   /*
+   This 
+   is 
+   a 
+   multiline
+   comment
+   */
+  /* escape sequence = character combination consisting of a backslash \
+                      follow by letter or combination of digits.
+                      They specify action within a liner or string of text.
+                      \n = newline
+                      \t = tab
+                      */
+   printf("I\nlike\npizza!\n");
+   printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n\n");
+   printf("\"Its really good\" - But now im bored of pizza\n\n");
+}
